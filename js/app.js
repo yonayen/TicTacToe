861cc2aadbe7927747bfa04cec7e3cbab74323c7
Yonathan Ayenew <yonayen@gmail.com>
2016-06-03 08:07:50 -0700

    add comments

--- js/app.js
+++ js/app.js
@@ -12,10 +12,25 @@ $(function() {
 			var pattern = definePatternForCurrentPlayer(player);
 			// Pattern for player is 'x' or 'o'
 			changeState(td, pattern);
+			// adds 'x' or 'o' to space
 			if(checkIfPlayerWon(table, pattern)) {
 				messages.html('Player ' + player + ' has won.');
 				turn.html('');
+			} else {
+				player = setNextPlayer(player);
+				displayNextPlayer(turn, player);
 			}
+		} else {
+			// if game hasn't been won and someone clicks on selected box
+			messages.html('This box is already checked.')
 		}
+	});
+
+	$('.reset').click(function() {
+
+	});
+
+		}
+			}
 	});
 });
