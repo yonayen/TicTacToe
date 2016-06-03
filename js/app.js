204a77ca295bbf3b4fd97a9251be67d79f9b5ced
Yonathan Ayenew <yonayen@gmail.com>
2016-06-03 07:49:42 -0700

    define functions for displaying 'x' or 'o'

--- js/app.js
+++ js/app.js
@@ -5,5 +5,17 @@ $(function() {
 	var turn = $('.turn');
 	displayNextPlayer(turn, player);
 
-	
+	$('td').click(function() {
+		td = $(this);
+		var state = getState(td);
+		if(!state) {
+			var pattern = definePatternForCurrentPlayer(player);
+			// Pattern for player is 'x' or 'o'
+			changeState(td, pattern);
+			if(checkIfPlayerWon(table, pattern)) {
+				messages.html('Player ' + player + ' has won.');
+				turn.html('');
+			}
+		}
+	});
 });
