fbff65c434867a50c28dba89a2c42fe95ab18cd2
Yonathan Ayenew <yonayen@gmail.com>
2016-06-07 23:50:05 -0700

    fix open/close brackets in js file

--- js/app.js
+++ js/app.js
@@ -5,24 +5,32 @@ $(function() {
   var table = $('table');
   var messages = $('.messages');
   var turn = $('.turn');
+  var gameOver = true;
   displayNextPlayer(turn, player);
   
+
   $('td').click(function() {
     td = $(this);
     var state = getState(td);
-    if(!state) {
+    if(!state) 
+    {
       var pattern = definePatternForCurrentPlayer(player);
 			// Pattern for player is 'x' or 'o'
 			changeState(td, pattern);
 			// adds 'x' or 'o' to space
-			if(checkIfPlayerWon(table, pattern)) {
+			if(checkIfPlayerWon(table, pattern))
+			{
 		        messages.html('Player '+player+' has won.');
 		        turn.html('');
-		      } else {
+		      }
+		      else
+		      {
 	        player = setNextPlayer(player);
 	        displayNextPlayer(turn, player);
+		      }
 		    }
-		    } else {
+		    else
+		    {
 		      messages.html('This box is already checked.');
 		    }
 		  });
